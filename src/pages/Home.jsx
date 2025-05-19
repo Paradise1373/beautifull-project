b41bd1fcf2ae20127cecdf493bd53adb90a00941
Abdullahmaghsoudi <abdullahmaghsoudi@gmail.com>
2025-05-19 19:01:30 +0330

    added starBackground folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import ThemeToggle from '../components/ThemeToggle'
+import StarBackground from '../components/StarBackground'
 
 const Home = () => {
   return (
     <div className='text-center min-h-screen bg-background text-foreground overflow-x-hidden'>
       <ThemeToggle />
+      <StarBackground />
     </div>
   )
 }
