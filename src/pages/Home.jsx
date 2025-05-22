1eacd29e947383fd9a9c8afb2419bb8d3cec1882
Abdullahmaghsoudi <abdullahmaghsoudi@gmail.com>
2025-05-22 13:27:40 +0330

    create aboutSection folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import ThemeToggle from '../components/ThemeToggle'
 import StarBackground from '../components/StarBackground'
 import NavBar from '../components/NavBar'
 import HeroSection from '../components/HeroSection'
+import AboutSection from '../components/AboutSection'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <StarBackground />
       <NavBar />
       <HeroSection />
+      <AboutSection />
     </div>
   )
 }
