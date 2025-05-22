62d7fc09013d91d70dc9cd5c16ab1a20ff798c37
Abdullahmaghsoudi <abdullahmaghsoudi@gmail.com>
2025-05-22 08:15:04 +0330

    complete desktop nav & mobile nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import ThemeToggle from '../components/ThemeToggle'
 import StarBackground from '../components/StarBackground'
+import NavBar from '../components/NavBar'
 
 const Home = () => {
   return (
-    <div className='text-center min-h-screen bg-background text-foreground overflow-x-hidden'>
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
       <ThemeToggle />
       <StarBackground />
+      <NavBar />
     </div>
   )
 }
