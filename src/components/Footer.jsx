944b687db1d06ab8af030c1658fd3891cb633313
Abdullahmaghsoudi <abdullahmaghsoudi@gmail.com>
2025-05-28 19:55:22 +0330

    added ContactSection folder

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      {' '}
+      &copy; {new Date().getFullYear()}
+      <p>ABdullahTech.co. All rights is reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
 import SkillsSection from '../components/SkillsSection'
 import ProjectsSection from '../components/ProjectsSection'
+import ContactSection from '../components/ContactSection'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -12,10 +14,14 @@ const Home = () => {
       <ThemeToggle />
       <StarBackground />
       <NavBar />
-      <HeroSection />
-      <AboutSection />
-      <SkillsSection />
-      <ProjectsSection />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+      <Footer />
     </div>
   )
 }
